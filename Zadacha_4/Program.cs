60616bf992ec838562507010036d2e3579f87079
Timofey_V <tim_vi_ov@mail.ru>
2022-11-23 23:43:28 +0300

    added solution to problem 4

--- Zadacha_4/Program.cs
+++ Zadacha_4/Program.cs
@@ -1,3 +1,10 @@
 ﻿// Напишите программу, которая на вход принимает число (N), 
 //а на выходе показывает все чётные числа от 1 до N.
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine());
+for(int i =1; i<=N; i++){
+if ((i % 2) == 0)
+ {
+Console.WriteLine(i);
+}
+}
