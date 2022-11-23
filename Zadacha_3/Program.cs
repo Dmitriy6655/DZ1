710673b4a1ec9305e63e6c8e9137d905c5e4805f
Timofey_V <tim_vi_ov@mail.ru>
2022-11-23 23:26:47 +0300

    added solution to problem 3

--- Zadacha_3/Program.cs
+++ Zadacha_3/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+if ((number%2) == 0){Console.WriteLine("Число четное");}
+else{Console.WriteLine("Число не четное");}
